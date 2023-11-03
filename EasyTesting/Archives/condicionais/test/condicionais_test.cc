a2d0a128a7119b2401fc1f80d258b6395b4f1d0d
ropnascimento <rafael.nascimento@fluna.io>
2023-11-02 22:11:24 -0300

    Alterações makefile

--- EasyTesting/Archives/condicionais/test/condicionais_test.cc
+++ EasyTesting/Archives/condicionais/test/condicionais_test.cc
@@ -3,17 +3,7 @@
 #include "test/condicionais_test.h"
 #include "../doctest/doctest.h"
 
-// Definição da função doctest::detail::MessageBuilder::MessageBuilder()
 
-doctest::detail::MessageBuilder::MessageBuilder(char const* file, int line, int assertionType) {
-    this->m_file = file;
-    this->m_line = line;
-    this->m_assertType = assertionType;
-}
-
-// Definition of the doctest::detail::MessageBuilder::~MessageBuilder() function
-
-doctest::detail::MessageBuilder::~MessageBuilder() {}
 
 int main(int argc, char** argv) {
     doctest::Context context;
